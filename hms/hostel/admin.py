2343482b676ceafe6c84c0148a1ca3bed565f9a2
faisalrasheed442@gmail.com <Sarata256>
2022-05-19 17:44:15 +0500

    room table created, rearrabged all the tables

--- hms/hostel/admin.py
+++ hms/hostel/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import customer
+from .models import customer,rooms,customer_fee
 # Register your models here.
 admin.site.register(customer)
+admin.site.register(rooms)
+admin.site.register(customer_fee)
--- hms/hostel/models.py
+++ hms/hostel/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 import time
 # Create your models here.
+class rooms(models.Model):
+    room_name=models.CharField(max_length=30)
+    room_id=models.AutoField(primary_key=True)
+    room_capacity=models.IntegerField(default=5)
+    def __str__(self):
+        return self.room_name
 
 class customer(models.Model):
     user_id=models.AutoField(primary_key=True)
@@ -8,7 +14,7 @@ class customer(models.Model):
     email=models.EmailField()
     password=models.CharField(max_length=30)
     contact=models.CharField(max_length=20,default="")
-    room=models.IntegerField(default=0)
+    room=models.ForeignKey(rooms,on_delete=models.CASCADE)
     gender=models.CharField(max_length=30,default='Not Mentioned')
     Guardian_name=models.CharField(max_length=100)
     Guardian_contact=models.CharField(max_length=20)
@@ -26,7 +32,5 @@ class customer_fee(models.Model):
     total_amount=models.IntegerField()
     allow_installment=models.BooleanField(default=True)
 
-class room(models.Model):
-    room_id=models.AutoField(primary_key=True)
-    room_capacity=models.IntegerField(default=5)
+
 
